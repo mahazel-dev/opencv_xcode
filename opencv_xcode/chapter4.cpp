e034bf30da7ed77f2d342db40415b2f25b982854
mahazel-dev <adam.kozlowski94@gmail.com>
2022-06-11 15:57:26 +0200

    added chapter 4

---
+++ opencv_xcode/chapter4.cpp
@@ -0,0 +1,31 @@
+//
+//  chapter4.cpp
+//  opencv_xcode
+//
+//  Created by Adam on 11/06/2022.
+//
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <iostream>
+#include <filesystem>
+
+
+int main () {
+    
+    
+    // Blank image
+    cv::Mat img(512, 512, CV_8UC3, cv::Scalar(255, 255, 255));
+    circle(img, cv::Point(256, 256), 155, cv::Scalar(0, 69, 255), 15);
+    
+    
+    
+    cv::imshow("Image", img);
+    
+    cv::waitKey(0);
+    
+    
+    
+}
